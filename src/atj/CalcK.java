8882f58f03ac3fd1f880368f09b2da932979d519
il <ilmak@o2.pl>
2020-06-16 20:01:34 +0200

    sam kalkulator // bez wygladu i bez przycisku S

---
+++ src/atj/CalcK.java
@@ -0,0 +1,217 @@
+package atj;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class CalcK implements Serializable {
+
+	private static final long serialVersionUID = -3512401082776903678L;
+
+	@PostConstruct
+	public void initialize() {
+		System.out.println(CalcK.class.getSimpleName() + " was constructed");
+	}
+
+	private String value = "0";
+	private String firstNumber = "";
+	private String secondNumber = "";
+	private String operation;
+
+	private boolean haveFirstNumber;
+	private boolean haveOperation;
+	private boolean afterEqual;
+
+	public void setValue(String v) {
+		value = v;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void addNewStringToNumberOne(String newInput) {
+		firstNumber += newInput;
+	}
+
+	public void addNewStringToNumberTwo(String newInput) {
+		secondNumber += newInput;
+	}
+
+	public void caclulate(String input) {
+
+		switch (input) {
+		case "C":
+			value = "0";
+			firstNumber = "";
+			secondNumber = "";
+			haveFirstNumber = false;
+			afterEqual = false;
+			break;
+
+		case "=":
+			if (haveFirstNumber && haveOperation && secondNumber.length() > 0) {
+
+				countResult(firstNumber, secondNumber, operation);
+				haveOperation = false;
+				firstNumber = value;
+				secondNumber = "";
+				afterEqual = true;
+
+			}
+			break;
+
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "0":
+			if (afterEqual) {
+				haveFirstNumber = false;
+				firstNumber = "";
+			}
+			if (!haveFirstNumber) {
+				addNewStringToNumberOne(input);
+				value = firstNumber;
+			} else {
+				addNewStringToNumberTwo(input);
+				value = firstNumber + " " + operation + " " + secondNumber;
+			}
+			afterEqual = false;
+			break;
+
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+			if (!haveOperation) {
+				operation = input;
+				haveOperation = true;
+				haveFirstNumber = true;
+				value = firstNumber + " " + operation;
+			} else if (haveFirstNumber && secondNumber.length() > 0) {
+				countResult(firstNumber, secondNumber, operation);
+				firstNumber = value;
+				haveFirstNumber = true;
+				operation = input;
+				value = firstNumber + " " + operation;
+				secondNumber = "";
+				haveOperation = true;
+			}
+			afterEqual = false;
+			break;
+
+		case ".":
+			if (!haveFirstNumber) {
+				if (firstNumber.isEmpty()) {
+					firstNumber = "0";
+				}
+				if (!firstNumber.contains(".")) {
+					addNewStringToNumberOne(input);
+					value = firstNumber;
+				}
+			} else {
+				if (secondNumber.isEmpty()) {
+					secondNumber = "0";
+				}
+				if (!secondNumber.contains(".")) {
+					addNewStringToNumberTwo(input);
+					value = firstNumber + " " + operation + " " + secondNumber;
+				}
+			}
+			afterEqual = false;
+			break;
+
+		case "sqrt":
+			value = formatValue(String.valueOf(new BigDecimal(Math.sqrt(Double.parseDouble(firstNumber)))));
+			firstNumber = value;
+			haveFirstNumber = true;
+			afterEqual = true;
+			break;
+
+		case "%":
+			if (!haveFirstNumber) {
+				if (firstNumber.isEmpty()) {
+					firstNumber = "0";
+				}
+				if (!value.contains("%")) {
+					value = firstNumber + "%";
+					firstNumber = String.valueOf(BigDecimal.valueOf(Double.parseDouble(firstNumber) / 100));
+					haveFirstNumber = true;
+					secondNumber = "0";
+					operation = "+";
+					haveOperation = true;
+				}
+			} else {
+				if (secondNumber.isEmpty()) {
+					secondNumber = "0";
+				}
+				if (!value.substring(value.length() - 1).equals("%")) {
+					value = firstNumber + " " + operation + " " + secondNumber + "%";
+					secondNumber = String.valueOf(new BigDecimal(firstNumber).multiply(new BigDecimal(secondNumber))
+							.divide(new BigDecimal(100)));
+
+				}
+			}
+			afterEqual = false;
+			break;
+
+		case "+/-":
+			value = formatValue(String.valueOf(new BigDecimal(firstNumber).multiply(new BigDecimal("-1"))));
+			firstNumber = value;
+			haveFirstNumber = true;
+			afterEqual = true;
+			break;
+
+		}
+	}
+
+	private void countResult(String firstNumber, String secondNumber, String operation) {
+
+		switch (operation) {
+		case "+":
+			value = formatValue(String.valueOf((new BigDecimal(firstNumber).add(new BigDecimal(secondNumber)))));
+			break;
+		case "-":
+			value = formatValue(String.valueOf((new BigDecimal(firstNumber).subtract(new BigDecimal(secondNumber)))));
+			break;
+		case "*":
+			value = formatValue(String.valueOf((new BigDecimal(firstNumber).multiply(new BigDecimal(secondNumber)))));
+			break;
+		case "/":
+			try {
+				value = formatValue(String.valueOf(
+						(new BigDecimal(firstNumber).divide(new BigDecimal(secondNumber), 8, RoundingMode.HALF_EVEN))));
+
+			} catch (ArithmeticException e) {
+				value = "ERR";
+			}
+			break;
+		}
+
+	}
+
+	private String formatValue(String inputValue) {
+
+		Locale.setDefault(new Locale("en", "US"));
+		NumberFormat format = new DecimalFormat("0.#######");
+		return format.format(new BigDecimal(inputValue));
+
+	}
+
+}
